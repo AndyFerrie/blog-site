0597544bc1832b36e269ad29e080ab0f47561ab2
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-09-28 17:13:25 +0100

    add 404 page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
 
 const ArticlePage = () => {
@@ -9,6 +10,9 @@ const ArticlePage = () => {
     // const articleId = params.articleId;
 
     const article = articles.find(article => article.name === articleId);
+
+    if (!article) return <NotFoundPage />
+    
     return (
         <>
             <h1>{article.title}</h1>
