2f4e1728d789d22ede7b55a5e2e2a215c5772582
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-09-28 15:16:58 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My blog</h1>
       <div id="page-body">
-        Welcome to my blog!
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles-list" element={<ArticlesListPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
