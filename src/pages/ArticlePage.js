721e15216413988de3caceb3a0e71eb9a1f6406a
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-10-09 19:40:23 +0100

    add useUser and amend ArticlePage per logged in

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 import articles from "./article-content";
 
 const ArticlePage = () => {
@@ -15,6 +16,8 @@ const ArticlePage = () => {
     // ^^ can also be written as:
     // const params = useParams();
     // const articleId = params.articleId;
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -39,15 +42,21 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
-                <button onClick={addUpvote}>Upvote</button>
+                {user
+                    ? <button onClick={addUpvote}>Upvote</button>
+                    : <button>Log in to upvote</button>
+                }
+                
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
-            <AddCommentForm
+            {user  
+                ? <AddCommentForm
                 articleName={articleId}
                 onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button>Log in to add a comment</button>}
             <CommentsList comments={articleInfo.comments} />
         </>
     );
