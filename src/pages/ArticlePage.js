92a02a5e327d9237bbd7d180fc297042e420c879
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-10-10 14:18:19 +0100

    add user tokens

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -21,7 +21,9 @@ const ArticlePage = () => {
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            const response = await axios.get(`/api/articles/${articleId}`, { headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
@@ -31,7 +33,9 @@ const ArticlePage = () => {
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
